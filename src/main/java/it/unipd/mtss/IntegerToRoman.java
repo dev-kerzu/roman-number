e67513837258358968b4fff3ac19885026973af3
dev-kerzu <xxxxxddddd89@gmail.com>
2026-05-03 16:29:49 +0200

    Update IntegerToRoman.java
    1 to 6

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,15 +3,29 @@
 // FABIO COSTA BANO 2137983
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
+
 public class IntegerToRoman {
+
     public static String convert(int number) {
         String result = "";
-    
-        // Logica semplicissima per far passare i test per i numeri 1, 2 e 3
+
+        // Gestione del simbolo 5 e successivi
+        while (number >= 5) {
+            result += "V";
+            number -= 5;
+        }
+
+        while (number >= 4) {
+            result += "IV";
+            number -= 4;
+        }
+
+        // Gestione dei numeri 1, 2, 3
         while (number >= 1) {
             result += "I";
             number -= 1;
         }
+
         return result;
     }
 }
