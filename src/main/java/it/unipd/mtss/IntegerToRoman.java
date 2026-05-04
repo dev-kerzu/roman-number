1ccad840a8a57ff6a8f496ff6aa5cdce21a0dfa5
dev-kerzu <xxxxxddddd89@gmail.com>
2026-05-04 19:24:45 +0200

    Update IntegerToRoman.java
    1 to 50

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,25 +7,43 @@ package it.unipd.mtss;
 public class IntegerToRoman {
 
     public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Numero non valido: " + number);
+        }
+
         String result = "";
-        
+
+        // Gestione del simbolo 50
+        while (number >= 50) {
+            result += "L";
+            number -= 50;
+        }
+
+        // Gestione del simbolo 40
+        while (number >= 40) {
+            result += "XL";
+            number -= 40;
+        }
+
         // Gestione del simbolo 10
         while (number >= 10) {
             result += "X";
             number -= 10;
         }
 
+        // Gestione del simbolo 9
         while (number >= 9) {
             result += "IX";
             number -= 9;
         }
-        
-        // Gestione del simbolo 5 e successivi
+
+        // Gestione del simbolo 5
         while (number >= 5) {
             result += "V";
             number -= 5;
         }
 
+        // Gestione del simbolo 4
         while (number >= 4) {
             result += "IV";
             number -= 4;
